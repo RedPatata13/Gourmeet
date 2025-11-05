5f47be9e86c33f8ccf68327d175d29e067fd380d
Red Alien <redpatatas13@gmail.com>
2025-11-05 21:07:15 +0800

    Basic Schema done

---
+++ database/migrations/2025_11_05_130444_create_reactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->id();
+
+            // Relationships
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->morphs('reactable'); // reactable_id + reactable_type
+
+            // Reaction details
+            $table->string('type'); // e.g. 'like', 'love', 'laugh', 'angry'
+
+            $table->timestamps();
+
+            $table->unique(['user_id', 'reactable_id', 'reactable_type']); // one reaction per user per item
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reactions');
+    }
+};

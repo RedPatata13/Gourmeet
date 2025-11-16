fdb710ae277e90ec648ea3818f1024f9892c759e
Rekkuso <rexguimespinosa060403@gmail.com>
2025-11-16 22:45:40 +0800

    HTTP fix v4

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <title>Register - Gourmeet</title>
     @vite('resources/css/app.css')
 </head>
@@ -194,13 +195,18 @@
             resetMessages();
             showLoading(true);
 
+            const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+            const url = new URL(window.location.origin + '/register');
+            url.protocol = 'https:';
+
             const formData = new FormData(this);
 
                         try {
-                            const response = await fetch(window.location.origin + '/register', {
+                            const response = await fetch(url, {
                                 method: 'POST',
                                 headers: {
-                                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                                    'X-CSRF-TOKEN': token
                                     'Accept': 'application/json'
                                 },
                                 body: formData  // Use FormData instead of JSON
